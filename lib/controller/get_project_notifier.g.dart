afee7adbbd4269234c75637f3bebfba2f08a02df
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-02-22 11:22:32 +0600

    added riverpod

---
+++ lib/controller/get_project_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_project_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getProjectNotifierHash() =>
+    r'd8086d33f47e1fc173da9fe0d7733a884723c04c';
+
+/// See also [GetProjectNotifier].
+@ProviderFor(GetProjectNotifier)
+final getProjectNotifierProvider =
+    AutoDisposeNotifierProvider<GetProjectNotifier, ProjectModel?>.internal(
+  GetProjectNotifier.new,
+  name: r'getProjectNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getProjectNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GetProjectNotifier = AutoDisposeNotifier<ProjectModel?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
